fe1cd3728bbb44cd8f21423e94b7b503b77bad07
San Shokunin <sanshokunin@gmail.com>
2013-05-19 19:05:40 +0200

    Added junit and scalatest dependencies.

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,9 @@ object FPInScalaBuild extends Build {
   val opts = Project.defaultSettings ++ Seq(
     scalaVersion := "2.10.0",
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-    libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.1.0"
+    libraryDependencies += "com.typesafe.akka" %% "akka-actor" % "2.1.0",
+    libraryDependencies += "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+    libraryDependencies += "junit" % "junit" % "4.10" % "test"
   )
 
   lazy val root =
