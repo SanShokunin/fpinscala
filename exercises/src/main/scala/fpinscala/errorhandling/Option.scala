98dfb4d791ab937924610cc7a1b1dada74aaad08
Shokunin San <sanshokunin@gmail.com>
2013-06-02 11:51:38 +0200

    Exercise 4.1: Removed implementations of Option from exerises.

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,18 +1,11 @@
 package fpinscala.errorhandling
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = this match {
-    case None => None
-    case Some(a) => Some(f(a))
-  }
+  def map[B](f: A => B): Option[B] = ???
   
-  def getOrElse[B>:A](default: => B): B = this match {
-    case None => default
-    case Some(a) => a
-  }
+  def getOrElse[B>:A](default: => B): B = ???
   
-  def flatMap[B](f: A => Option[B]): Option[B] = 
-    map(f) getOrElse None
+  def flatMap[B](f: A => Option[B]): Option[B] = ???
   
   /*
   Of course, we can also implement `flatMap` with explicit pattern matching.
@@ -22,8 +15,7 @@ sealed trait Option[+A] {
     case Some(a) => f(a)
   }
   
-  def orElse[B>:A](ob: => Option[B]): Option[B] = 
-    this map (Some(_)) getOrElse ob
+  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
   
   /*
   Again, we can implement this with explicit pattern matching. 
@@ -33,16 +25,15 @@ sealed trait Option[+A] {
     case _ => this
   }
   
-  def filter(f: A => Boolean): Option[A] = this match {
-    case Some(a) if f(a) => this
-    case _ => None
-  }
+  def filter(f: A => Boolean): Option[A] = ???
+
   /*
   This can also be defined in terms of `flatMap`.
   */
   def filter_1(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
 }
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
