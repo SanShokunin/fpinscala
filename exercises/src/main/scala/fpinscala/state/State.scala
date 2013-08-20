cdb6c23b8db8ab96042a38b671671d3125a336d2
Shokunin San <me@flatmap.io>
2013-08-20 11:40:16 +0200

    Exercises 6.{1,2,3,4}: Implemented methods for generating random numbers by a RNG.

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -28,17 +28,80 @@ object RNG {
       (f(a), rng2)
     }
 
-  def positiveInt(rng: RNG): (Int, RNG) = sys.error("todo")
+  /**
+   * Exercise 6.1:
+   *
+   * Write a function to generate a random positive integer. Note: you can use
+   * x.abs to take the absolute value of an Int, x. Make sure to handle the
+   * corner case Int.MinValue, which doesn't have a positive counterpart.
+   *
+   * @param rng Random Number Generator [RNG]
+   * @return Tuple of generated positive Integer and a new RNG
+   */
+  def positiveInt(rng: RNG): (Int, RNG) = {
+    val (next, newRng) = rng.nextInt
+    if (next == Int.MinValue) positiveInt(newRng) else (next.abs, newRng)
+  }
 
-  def double(rng: RNG): (Double, RNG) = sys.error("todo")
+  /**
+   * Exercise 6.2:
+   *
+   * Write a function to generate a Double between 0 and 1, not including 1.
+   *
+   * Note: you can use Int.MaxValue to obtain the maximum positive integer value
+   * and you can use x.toDouble to convert an Int, x, to a Double.
+   *
+   * @param rng Random Number Generator [RNG]
+   * @return Tuple of generated Double and a new RNG
+   */
+  def double(rng: RNG): (Double, RNG) = {
+    val (next, newRng) = rng.nextInt
+    (next / (Int.MaxValue.toDouble + 1), newRng)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = sys.error("todo")
+  /**
+   * Exercise 6.3:
+   *
+   * Write functions to generate an (Int, Double) pair, a (Double, Int) pair,
+   * and a (Double, Double, Double) 3-tuple. You should be able to reuse the
+   * functions you've already written.
+   */
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, newRng) = rng.nextInt
+    val (d, newRng2) = double(newRng)
+    ((i, d), newRng2)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = sys.error("todo")
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i, d), newRng) = intDouble(rng)
+    ((d, i), newRng)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = sys.error("todo")
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, newRng) = double(rng)
+    val (d2, newRng2) = double(newRng)
+    val (d3, newRng3) = double(newRng2)
+    ((d1, d2, d3), newRng3)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = sys.error("todo")
+  /**
+   * Exercise 6.4:
+   *
+   * Write a function to generate a list of random integers.
+   *
+   * @param count
+   * @param rng
+   * @return
+   */
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(count: Int, rng: RNG, ints: List[Int]): (List[Int], RNG) =
+      if (count == 0) (ints, rng)
+      else {
+        val (i, r) = rng.nextInt
+        go(count - 1, r, i :: ints)
+      }
+    go(count, rng, Nil)
+  }
 
   def positiveMax(n: Int): Rand[Int] = sys.error("todo")
 
@@ -47,6 +110,7 @@ object RNG {
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = sys.error("todo")
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = sys.error("todo")
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
