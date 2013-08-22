3f8e59f7af8db2959cc295ca56f819d794ab9bb9
Shokunin San <me@flatmap.io>
2013-08-22 20:26:14 +0200

    Exercise 6.13: Reformatted code a little.

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -353,13 +353,17 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 object Candy {
 
   def simulateMachine(inputs: List[Input]): State[Machine, Int] = for {
-    _ <- State.sequence(inputs.map(i => modify((s: Machine) => (i, s) match {
-           case (_, Machine(_, 0, _)) => s
-           case (Coin, Machine(false, _, _)) => s
-           case (Turn, Machine(true, _, _)) => s
-           case (Coin, Machine(true, candy, coin)) => Machine(false, candy, coin + 1)
-           case (Turn, Machine(false, candy, coin)) => Machine(true, candy - 1, coin)
-         })))
+    _ <- State.sequence(
+           inputs.map(i =>
+             modify((s: Machine) => (i, s) match {
+               case (_, Machine(_, 0, _)) => s
+               case (Coin, Machine(false, _, _)) => s
+               case (Turn, Machine(true, _, _)) => s
+               case (Coin, Machine(true, candy, coin)) => Machine(false, candy, coin + 1)
+               case (Turn, Machine(false, candy, coin)) => Machine(true, candy - 1, coin)
+             })
+           )
+         )
     s <- get
   } yield s.coins
 
